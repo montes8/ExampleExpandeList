fb010a75f4d69a5a968e90603d449394defeec18
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-08-26 07:53:40 -0500

    update example

--- app/src/main/java/com/gb/vale/demoexpandelist/MainActivity.java
+++ app/src/main/java/com/gb/vale/demoexpandelist/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 
 import com.gb.vale.demoexpandelist.adapter.ParentMedicAdapter;
 import com.gb.vale.demoexpandelist.model.ParentMedic;
+import com.gb.vale.demoexpandelist.presenter.IMainPresenter;
+import com.gb.vale.demoexpandelist.presenter.PresenterMain;
 
 import java.util.List;
 
@@ -16,17 +20,21 @@ public class MainActivity extends AppCompatActivity implements IMainPresenter {
 
     ParentMedicAdapter adapterParent = new ParentMedicAdapter();
     private  RecyclerView rvParent;
-    private PresenterMain  presenterMain;
+
+    private RadioGroup rbSelector;
+    private PresenterMain presenterMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rvParent = findViewById(R.id.rvClinic);
+        rbSelector = findViewById(R.id.rgSelected);
         rvParent.setLayoutManager(new LinearLayoutManager(this));
         rvParent.setAdapter(adapterParent);
         presenterMain = new PresenterMain(this);
-        selectedDate(2);
+        selectedDate(1);
+        configRadioSelector();
     }
 
     public void selectedDate(int date){
@@ -35,12 +43,25 @@ public class MainActivity extends AppCompatActivity implements IMainPresenter {
 
     @Override
     public void successListParent(List<ParentMedic> list, int value) {
-        presenterMain.getListHistory(list,value);
-    }
-
-    @Override
-    public void successListHistory(List<ParentMedic> list) {
         rvParent.setVisibility(list.isEmpty()?View.GONE : View.VISIBLE);
         adapterParent.addList(list);
     }
+    @SuppressLint("NonConstantResourceId")
+    private void configRadioSelector() {
+        rbSelector.setOnCheckedChangeListener((group, checkedId) -> {
+            switch (checkedId) {
+                case R.id.rbOne: { selectedDate(1);}
+                break;
+                case R.id.rbTwo : { selectedDate(2);}
+                break;
+                case R.id.rbThree :{ selectedDate(3);}
+                break;
+                case R.id.rbFour: { selectedDate(4);}
+                break;
+                case R.id.rbSix: { selectedDate(5);}
+                break;
+        }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/gb/vale/demoexpandelist/presenter/IMainPresenter.java
@@ -0,0 +1,9 @@
+package com.gb.vale.demoexpandelist.presenter;
+
+import com.gb.vale.demoexpandelist.model.ParentMedic;
+
+import java.util.List;
+
+public interface IMainPresenter {
+    void successListParent(List<ParentMedic> list, int value);
+}

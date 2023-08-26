3e6acfbc3e5de73bb2381f7aa1071f58a3ab9b9b
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-08-26 01:43:06 -0500

    list

--- app/src/main/java/com/gb/vale/demoexpandelist/MainActivity.java
+++ app/src/main/java/com/gb/vale/demoexpandelist/MainActivity.java
@@ -1,14 +1,46 @@
 package com.gb.vale.demoexpandelist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.gb.vale.demoexpandelist.adapter.ParentMedicAdapter;
+import com.gb.vale.demoexpandelist.model.ParentMedic;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements IMainPresenter {
+
+    ParentMedicAdapter adapterParent = new ParentMedicAdapter();
+    private  RecyclerView rvParent;
+    private PresenterMain  presenterMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvParent = findViewById(R.id.rvClinic);
+        rvParent.setLayoutManager(new LinearLayoutManager(this));
+        rvParent.setAdapter(adapterParent);
+        presenterMain = new PresenterMain(this);
+        selectedDate(2);
+    }
+
+    public void selectedDate(int date){
+        presenterMain.getListParent(date);
+    }
+
+    @Override
+    public void successListParent(List<ParentMedic> list, int value) {
+        presenterMain.getListHistory(list,value);
+    }
+
+    @Override
+    public void successListHistory(List<ParentMedic> list) {
+        rvParent.setVisibility(list.isEmpty()?View.GONE : View.VISIBLE);
+        adapterParent.addList(list);
     }
 }
